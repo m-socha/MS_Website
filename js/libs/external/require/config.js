93c21ce3c8356f92bd3fd5ecb15c2be05d2adb4b
Michael Socha <msocha@uwaterloo.ca>
2017-08-29 14:36:50 -0400

    Add external libs and setup project structure.

---
+++ js/libs/external/require/config.js
@@ -0,0 +1,16 @@
+requirejs.config({
+  baseUrl: 'js',
+  paths: {
+    text: 'libs/external/require/plugins/textJS',
+    css: 'libs/external/require/plugins/css.min',
+    jquery: 'libs/external/jquery/jquery-3.2.1.min',
+    underscore: 'libs/external/underscore/underscore-1.8.3.min',
+    backbone: 'libs/external/backbone/backbone-1.3.3.min',
+    index: 'index'
+  },
+  shim: {
+    backbone: ['underscore']
+  }
+});
+
+requirejs(['index']);
